5e0e19e1288e009b589354d9e2982ff7cd8c3c84
Ubuntu <ubuntu@ip-172-31-18-86.us-east-2.compute.internal>
2026-05-15 20:22:35 +0000

    Added Terraform IaC

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "devsecops_sg" {
+  name        = "enterprise-devsecops-sg"
+  description = "Security Group for Enterprise DevSecOps Pipeline"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Development Environment"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Staging Environment"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Production Environment"
+    from_port   = 3002
+    to_port     = 3002
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Enterprise-DevSecOps-SG"
+  }
+}
