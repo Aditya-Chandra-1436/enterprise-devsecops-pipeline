500f5fb1567be8db0ce64a93c0ec8fcbf76f5d73
Ubuntu <ubuntu@ip-172-31-18-86.us-east-2.compute.internal>
2026-05-15 21:28:58 +0000

    Fixed Terraform deployment AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,61 +1,103 @@
-#checkov:skip=CKV2_AWS_5: Security group attached manually to existing EC2 instance
+resource "aws_key_pair" "deploy_key" {
 
-resource "aws_security_group" "devsecops_sg" {
-  name        = "enterprise-devsecops-sg-v1"
-  description = "Security Group for Enterprise DevSecOps Pipeline"
+  key_name   = "devsecops-deploy-key"
+
+  public_key = file("../terraform-keys/devsecops-deploy-key.pub")
+}
+
+resource "aws_security_group" "deploy_sg" {
+
+  name        = "enterprise-devsecops-deploy-sg"
+
+  description = "Security Group for Deployment EC2"
 
   ingress {
-    description = "SSH Access Restricted"
+
+    description = "SSH Access"
 
     from_port   = 22
+
     to_port     = 22
+
     protocol    = "tcp"
 
-    # REPLACE WITH YOUR ACTUAL PUBLIC IP
-    cidr_blocks = ["18.224.8.10/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+
     description = "Development Environment"
 
     from_port   = 3000
+
     to_port     = 3000
+
     protocol    = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+
     description = "Staging Environment"
 
     from_port   = 3001
+
     to_port     = 3001
+
     protocol    = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+
     description = "Production Environment"
 
     from_port   = 3002
+
     to_port     = 3002
+
     protocol    = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    description = "HTTPS Outbound Traffic"
+
+    description = "HTTPS Outbound"
 
     from_port   = 443
+
     to_port     = 443
+
     protocol    = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Enterprise-DevSecOps-SG"
+
+    Name = "Enterprise-DevSecOps-Deploy-SG"
+  }
+}
+
+resource "aws_instance" "deploy_server" {
+
+  ami           = "ami-0e86e20dae9224db8"
+
+  instance_type = "t3.micro"
+
+  key_name = aws_key_pair.deploy_key.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.deploy_sg.id
+  ]
+
+  associate_public_ip_address = true
+
+  tags = {
+
+    Name = "Enterprise-DevSecOps-Deploy-Server"
   }
 }
